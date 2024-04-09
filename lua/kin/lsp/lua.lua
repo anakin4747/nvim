906924e20a700365c1d1bcf3ccf86e378583785a
Anakin Childerhose <anakin@childerhose.ca>
2024-04-09 09:05:27 -0400

    added rename lsp feature to lua

--- lua/kin/lsp/lua.lua
+++ lua/kin/lsp/lua.lua
@@ -30,5 +30,6 @@ require('lspconfig').lua_ls.setup({
         vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
         vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
     end,
 })
