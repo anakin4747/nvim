9bc33dde447f6e048e37d7b27919b6e69fb97a49
Anakin Childerhose <anakin@childerhose.ca>
2024-04-20 00:13:28 -0400

    Moved plugins.lua to the top
    Needed packer to run before other plugins so I moved it to the top. This
    prevents an error from starting with a new config not working

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
+require("kin.plugins")
+
 require("kin.autocmds")
 require("kin.cmp")
 require("kin.colorscheme")
 require("kin.keymaps")
 require("kin.lsp")
 require("kin.options")
-require("kin.plugins")
 require("kin.telescope")
 require("kin.treesitter")
